03a19aaafbc336186bd5a0ef801d8aebdb1b12d9
Muhammad Zeeshan <muhammadzeeshanlilla2002@gmail.com>
2026-05-31 11:51:15 +0500

    feat: register company and user models in admin

--- apps/accounts/admin.py
+++ apps/accounts/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import User
+
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    list_display = ("id", "username", "email", "company", "role", "is_active", "is_deleted")
+    list_filter = ("role", "is_active", "is_deleted", "company")
+    search_fields = ("username", "email")
+    ordering = ("-created_at",)
+
+    fieldsets = UserAdmin.fieldsets + (
+        ("Company & Role Info", {
+            "fields": ("company", "role", "is_deleted")
+        }),
+    )
+
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ("Company & Role Info", {
+            "fields": ("company", "role", "is_deleted")
+        }),
+    )
--- apps/companies/admin.py
+++ apps/companies/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Company
 
-# Register your models here.
+
+@admin.register(Company)
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "slug", "email", "is_active", "created_at")
+    search_fields = ("name", "email", "slug")
+    list_filter = ("is_active", "created_at")
+    readonly_fields = ("created_at", "updated_at")
